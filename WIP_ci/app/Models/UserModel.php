7b6d02c9996f4675f848948f3255e181a867c1c7
Payal Pochampalliwar <147625519+Payalpochampalliwar@users.noreply.github.com>
2025-06-30 17:02:03 +0530

    Uploaded New Files

--- WIP_ci/app/Models/UserModel.php
+++ WIP_ci/app/Models/UserModel.php
@@ -11,7 +11,7 @@ class UserModel extends Model{
     protected $allowedFields = [
     'User_Id', 'User_FirstName', 'User_LastName', 'User_Gender', 'User_DOB',
     'User_FatherName', 'User_MotherName', 'User_Aadhar_No', 'User_Phone_No',
-    'User_Village/City', 'User_District', 'User_State', 'User_Nationality',
+    'User_Village_City', 'User_District', 'User_State', 'User_Nationality',
     'User_Joining_Date', 'User_Photo', 'User_Aadhar_Photo', 'User_Address',
     'User_Status', 'Role_Id', 'Program_Id', 'Record_Added_By', 'Rec_Added_On',
     'Rec_Updated_By', 'Rec_Last_Updated_On'
