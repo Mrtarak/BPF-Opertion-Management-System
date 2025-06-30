31f576ecd8dba8849d7b9158be7b64a00bbbd845
Payal Pochampalliwar <147625519+Payalpochampalliwar@users.noreply.github.com>
2025-06-30 15:52:05 +0530

    Uploaded New Files

--- WIP_ci/app/Views/ManageUsers/edit_user.php
+++ WIP_ci/app/Views/ManageUsers/edit_user.php
@@ -34,83 +34,83 @@
   <div class="row">
     <div class="col-md-6 form-group">
       <label> First Name</label>
-      <input type="text" name="User_FirstName" class="form-control" value="<?= old('User_FirstName') ?>" placeholder="Enter First Name" >
+      <input type="text" name="User_FirstName" class="form-control" value="<?= esc($user['User_FirstName']) ?>" placeholder="Enter First Name" >
     </div>
 
     <div class="col-md-6 form-group">
       <label> Last Name</label>
-      <input type="text" name="User_LastName" class="form-control" value="<?= old('User_LastName') ?>" placeholder="Enter Last Name" >
+      <input type="text" name="User_LastName" class="form-control" value="<?= esc($user['User_LastName']) ?>" placeholder="Enter Last Name" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Gender</label>
       <select class="form-select" name="User_Gender" >
     <option value="">Select Gender</option>
-    <option value="Male" <?= old('User_Gender') == 'Male' ? 'selected' : '' ?>>Male</option>
-    <option value="Female" <?= old('User_Gender') == 'Female' ? 'selected' : '' ?>>Female</option>
-    <option value="Other" <?= old('User_Gender') == 'Other' ? 'selected' : '' ?>>Other</option>
+    <option value="Male" <?= esc($user['User_Gender']) == 'Male' ? 'selected' : '' ?>>Male</option>
+    <option value="Female" <?= esc($user['User_Gender']) == 'Female' ? 'selected' : '' ?>>Female</option>
+    <option value="Other" <?= esc($user['User_Gender']) == 'Other' ? 'selected' : '' ?>>Other</option>
 </select>
     </div>
     </div>
 
     <div class="col-md-6 form-group">
       <label>DOB</label>
-      <input type="date" name="User_DOB" class="form-control" value="<?= old('User_DOB') ?>">
+      <input type="date" name="User_DOB" class="form-control" value="<?= esc($user['User_DOB']) ?>">
     </div>
 
     <div class="col-md-6 form-group">
       <label>Father Name</label>
-      <input type="text" name="User_FatherName" class="form-control" value="<?= old('User_FatherName') ?>" placeholder="Enter Father Name" >
+      <input type="text" name="User_FatherName" class="form-control" value="<?= esc($user['User_FatherName']) ?>" placeholder="Enter Father Name" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Mother Name</label>
-      <input type="text" name="User_MotherName" class="form-control" value="<?= old('User_MotherName') ?>" placeholder="Enter Mother Name" >
+      <input type="text" name="User_MotherName" class="form-control" value="<?= esc($user['User_MotherName']) ?>" placeholder="Enter Mother Name" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Aadhar Number</label>
-      <input type="text" name="User_Aadhar_No" class="form-control" value="<?= old('User_Aadhar_No') ?>" placeholder="Enter Aadhar Number" >
+      <input type="text" name="User_Aadhar_No" class="form-control" value="<?= esc($user['User_Aadhar_No']) ?>" placeholder="Enter Aadhar Number" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Phone Number</label>
-      <input type="text" name="User_Phone_No" class="form-control" value="<?= old('User_Phone_No') ?>" placeholder="Enter Phone Number" >
+      <input type="text" name="User_Phone_No" class="form-control" value="<?= esc($user['User_Phone_No']) ?>" placeholder="Enter Phone Number" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Village/City</label>
-      <input type="text" name="User_Village_City" class="form-control" value="<?= old('User_Village_City') ?>" placeholder="Enter Village/City" >
+      <input type="text" name="User_Village_City" class="form-control" value="<?= esc($user['User_Village_City']) ?>" placeholder="Enter Village/City" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>District</label>
-      <input type="text" name="User_District" class="form-control" value="<?= old('User_District') ?>" placeholder="Enter District" >
+      <input type="text" name="User_District" class="form-control" value="<?= esc($user['User_District']) ?>" placeholder="Enter District" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>State</label>
-      <input type="text" name="User_State" class="form-control" value="<?= old('User_State') ?>" placeholder="Enter State" >
+      <input type="text" name="User_State" class="form-control" value="<?= esc($user['User_State']) ?>" placeholder="Enter State" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Nationality</label>
-      <input type="text" name="User_Nationality	" class="form-control" value="<?= old('User_Nationality	') ?>" placeholder="Enter Nationality" >
+      <input type="text" name="User_Nationality	" class="form-control" value="<?= esc($user['User_Nationality']) ?>" placeholder="Enter Nationality" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Joining Date</label>
-      <input type="date" name="	User_Joining_Date" class="form-control" value="<?= old('User_Joining_Date') ?>" >
+      <input type="date" name="	User_Joining_Date" class="form-control" value="<?= esc($user['User_Joining_Date']) ?>" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Profile Photo</label>
-      <input type="file" name="User_Photo" class="form-control" value="<?= old('User_Photo') ?>" placeholder="Upload Your Photo" >
+      <input type="file" name="User_Photo" class="form-control" value="<?= esc($user['User_Photo']) ?>" placeholder="Upload Your Photo" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Aadhar Photo</label>
-      <input type="file" name="User_Aadhar_Photo" class="form-control" value="<?= old('User_Aadhar_Photo') ?>" placeholder="Upload Aadhar Photo" >
+      <input type="file" name="User_Aadhar_Photo" class="form-control" value="<?= esc($user['User_Aadhar_Photo']) ?>" placeholder="Upload Aadhar Photo" >
     </div>
 
     <div class="col-md-6 form-group">
@@ -143,37 +143,37 @@
 
     <div class="col-md-6 form-group">
       <label>Address</label>
-      <input type="text" name="User_Address" class="form-control" value="<?= old('User_Address') ?>" placeholder="Enter Address" >
+      <input type="text" name="User_Address" class="form-control" value="<?= esc($user['User_Address']) ?>" placeholder="Enter Address" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Status</label>
       <select class="form-select" name="User_Status" >
     <option value="">Select Status</option>
-    <option value="Active" <?= old('User_Status') == 'Active' ? 'selected' : '' ?>>Active</option>
-    <option value="InActive" <?= old('User_Status') == 'InActive' ? 'selected' : '' ?>>InActive</option>
-    <option value="Completed" <?= old('User_Status') == 'Completed' ? 'selected' : '' ?>>Completed</option>
+    <option value="Active" <?= esc($user['User_Status']) == 'Active' ? 'selected' : '' ?>>Active</option>
+    <option value="InActive" <?= esc($user['User_Status']) == 'InActive' ? 'selected' : '' ?>>InActive</option>
+    <option value="Completed" <?= esc($user['User_Status']) == 'Completed' ? 'selected' : '' ?>>Completed</option>
 </select>
     </div>
 
     <div class="col-md-6 form-group">
       <label>Recorded By</label>
-      <input type="text" name="Record_Added_By" class="form-control" value="<?= old('Record_Added_By') ?>" placeholder="Enter Who Recorded Record" >
+      <input type="text" name="Record_Added_By" class="form-control" value="<?= esc($user['Record_Added_By']) ?>" placeholder="Enter Who Recorded Record" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Recorded On</label>
-      <input type="date" name="Rec_Added_On" class="form-control" value="<?= old('Rec_Added_On') ?>">
+      <input type="date" name="Rec_Added_On" class="form-control" value="<?= esc($user['Rec_Added_On']) ?>">
     </div>
 
     <div class="col-md-6 form-group">
       <label>Updated By</label>
-      <input type="text" name="Rec_Updated_By" class="form-control" value="<?= old('Rec_Updated_By') ?>" placeholder="Enter Who Updated Record" >
+      <input type="text" name="Rec_Updated_By" class="form-control" value="<?= esc($user['Rec_Updated_By']) ?>" placeholder="Enter Who Updated Record" >
     </div>
 
     <div class="col-md-6 form-group">
       <label>Last Updated On</label>
-      <input type="date" name="Rec_Last_Updated_On" class="form-control" value="<?= old('Rec_Last_Updated_On') ?>">
+      <input type="date" name="Rec_Last_Updated_On" class="form-control" value="<?= esc($user['Rec_Last_Updated_On']) ?>">
     </div>
   </div>
 
